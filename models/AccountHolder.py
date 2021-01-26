993eb9d7090ad820a6bae7aea35d9293de9b6367
laken11 <olaniteolalekan@gmail.com>
2021-01-26 12:13:44 +0100

    My first commiti

---
+++ models/AccountHolder.py
@@ -0,0 +1,16 @@
+class AccountHolder:
+    def __init__(self):
+        self.first_name = None
+        self.middle_name = None
+        self.last_name = None
+        self.email = email
+        self.password = password
+        self.phone = None
+
+
+owner = AccountHolder()
+
+owner.email = 'olaniteolalekan@gmail.com'
+owner.password = '363647492'
+
+print(owner.email)
